78c1a48dca4e1710287735cb914337755aaf778b
Natannegara <natannegara@gmail.com>
2020-10-22 09:34:38 +0700

    [remove] register and login

--- register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express.Router()
-const db = require('./controller/dbController')
-
-app.post('/register', (req, res) => {
-    res.send(db.add('users', req.body))
-})
-
-module.exports = app
