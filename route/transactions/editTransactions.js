bd2006402a0bd78f5384e7bf7969a341159ba238
Natannegara <72841898+Natannegara@users.noreply.github.com>
2020-10-22 09:40:39 +0700

    Update editTransactions.js

--- route/transactions/editTransactions.js
+++ route/transactions/editTransactions.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express.Router()
 const db = require('../../controller/dbController')
-const accessData = require('../../middleWare/authentication')
 
-app.use(accessData)
 app.patch('/transactions', (req, res) => {
     const id = req.query.id
     const body = req.body
