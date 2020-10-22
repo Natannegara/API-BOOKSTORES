8980f23de168cef2cb1b55d5bb972bfe3810ba82
Natannegara <72841898+Natannegara@users.noreply.github.com>
2020-10-22 09:42:02 +0700

    deleted middleware

--- route/transactions/getTransactions.js
+++ route/transactions/getTransactions.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express.Router()
 const db = require('../../controller/dbController')
-const accessData = require('../../middleWare/authentication')
-
-app.use(accessData)
 
 app.get('/transactions', (req, res) => {
     const parseId = parseInt(req.body.id)
