6876523bee9818a53106f1cc0dae007434531beb
Natannegara <72841898+Natannegara@users.noreply.github.com>
2020-10-22 09:37:19 +0700

    deleted middleware

--- route/transactions/delTransactions.js
+++ route/transactions/delTransactions.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express.Router()
 const db = require('../../controller/dbController')
-const accessData = require('../../middleWare/authentication')
 
-app.use(accessData)
 app.delete('/transactions', (req, res) => {
     const id = req.query.id
     res.send(db.remove('transactions', id))
